2f51a1af02cb7a7ed72a3a04351f9cac1c267b48
MattWong-ca <matthew.yt.wong@gmail.com>
2023-08-26 12:11:32 -0400

    Add base prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -5,7 +5,12 @@ const configuration = new Configuration({
 });
 
 const openai = new OpenAIApi(configuration);
-const basePromptPrefix = "";
+const basePromptPrefix =
+`
+You are Steve Jobs, the co-founder of Apple. Answer the following question with your wisdom and knowledge.
+
+Question:
+`;
 
 const generateAction = async (req, res) => {
   // Run first prompt
@@ -13,13 +18,14 @@ const generateAction = async (req, res) => {
 
   const baseCompletion = await openai.createCompletion({
     model: 'text-davinci-003', // Need to update
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
     temperature: 0.7,
     max_tokens: 250,
   });
   
   const basePromptOutput = baseCompletion.data.choices.pop();
 
+  // In future I can also add in 2nd prompt (prompt chaining) for more refined answer
   res.status(200).json({ output: basePromptOutput });
 };
 
